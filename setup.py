8733df1ebb2fc1526e68af5aeee56e5c00fee309
AlexanderBlaies <alexanderblaies@AlexanderBlaiess-MacBook-Pro.local>
2021-02-08 11:22:45 -0600

    new repo for the 2nd Capstone Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project was proposed as part of the Springboard Curriculum; this project's purpose is to understand if one can predict the real estate market by zip code in a city (preferrably Houston)',
+    author='Alexander Blaies',
+    license='',
+)
